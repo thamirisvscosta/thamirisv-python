87049217634595bcdee67b94125cf83a4d7ad2a0
Thamiris Costa <94200167+thamirisvscosta@users.noreply.github.com>
2023-08-15 19:37:14 -0300

    exercicio do hd do notebook
    exercicio para downloads do hd do notebook

---
+++ exercicios-praticos/yw.py
@@ -0,0 +1,23 @@
+from pytube import YouTube
+import moviepy.editor as mp
+import os
+
+# Insira o link do vídeo que você deseja baixar
+link = "https://www.youtube.com/watch?v=nl71vFvVOvw"
+
+# Cria uma instância do objeto YouTube
+yt = YouTube(link)
+
+# Obtém a stream de áudio com a melhor qualidade
+audio_stream = yt.streams.filter(only_audio=True).first()
+
+# Baixa o arquivo de áudio em formato mp4
+audio_file = audio_stream.download()
+
+# Converte o arquivo de áudio mp4 para mp3
+audio_clip = mp.AudioFileClip(audio_file)
+audio_clip.write_audiofile(audio_file.replace(".mp4", ".mp3"))
+
+# Remove o arquivo de áudio mp4
+audio_clip.close()
+os.remove(audio_file)
