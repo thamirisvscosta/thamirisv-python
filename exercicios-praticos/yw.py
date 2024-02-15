cd5b36ad891e81863dcfbbf3765455c880f41d68
Thamiris Costa <thamirisvscosta@gmail.com>
2024-02-15 13:37:42 -0300

    nada de novo ...

--- exercicios-praticos/yw.py
+++ exercicios-praticos/yw.py
@@ -3,7 +3,7 @@ import moviepy.editor as mp
 import os
 
 # Insira o link do vídeo que você deseja baixar
-link = "https://www.youtube.com/watch?v=hU87SWQdewI"
+link = "https://www.youtube.com/watch?v=YpugK0RpEaU"
 
 # Cria uma instância do objeto YouTube
 yt = YouTube(link)
